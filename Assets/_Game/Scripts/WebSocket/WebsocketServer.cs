8bba0ce1d8f936e3c8f1477d13868b4fd49f10cd
Duchuy <huy.hd@rubycell.com>
2021-01-17 12:02:14 +0700

    chat connect + receive message

---
+++ Assets/_Game/Scripts/WebSocket/WebsocketServer.cs
@@ -0,0 +1,78 @@
+﻿using NativeWebSocket;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+// Link github Native websocket: https://github.com/endel/NativeWebSocket
+public class WebsocketServer : MonoBehaviour {
+    public static WebsocketServer Instance;
+    //wss://https://chatgolang.herokuapp.com/ws/roomId/userId
+
+    public static string SERVER_URL = "wss://chatgolang.herokuapp.com/ws/" + ROOM_ID + "/" + USER_ID;
+    public static string ROOM_ID = "main";
+    public static string USER_ID = "user";
+    public static float CHAT_HEIGHT = 100f;
+
+    public TMP_InputField chatInput;
+    public GameObject chatObj;
+    public Transform content;
+
+    WebSocket websocket;
+
+    private void Awake() {
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    async void Start() {
+        websocket = new WebSocket(SERVER_URL);
+
+        websocket.OnOpen += () => {
+            Debug.Log("Connection open!");
+        };
+
+        websocket.OnError += (e) => {
+            Debug.Log("Error! " + e);
+        };
+
+        websocket.OnClose += (e) => {
+            Debug.Log("Connection closed!");
+        };
+
+        websocket.OnMessage += (bytes) => {
+            var message = System.Text.Encoding.UTF8.GetString(bytes);
+
+            Debug.Log("Receive Message: " + message);
+
+            GameObject go = Instantiate(chatObj, content);
+            Vector2 size = content.GetComponent<RectTransform>().sizeDelta;
+            content.GetComponent<RectTransform>().sizeDelta = new Vector2(size.x, size.y + CHAT_HEIGHT);
+        };
+
+        //InvokeRepeating("SendWebSocketMessage", 0.0f, 0.3f);
+
+        await websocket.Connect();
+    }
+
+    void Update() {
+#if !UNITY_WEBGL || UNITY_EDITOR
+        websocket.DispatchMessageQueue();
+#endif
+    }
+
+    public void SendChat() {
+        SendWebSocketMessage();
+    }
+
+    async void SendWebSocketMessage() {
+        if (websocket.State == WebSocketState.Open && chatInput.text != "") {
+            //string message = chatInput.text;
+            await websocket.SendText(chatInput.text);
+        }
+    }
+
+    private async void OnApplicationQuit() {
+        await websocket.Close();
+    }
+}
